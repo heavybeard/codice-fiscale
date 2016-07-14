c54b5fb5e2b7fd9d7623e1e69049de53e337ea9b
Andrea Cognini <andrea.cognini@triboo.it>
2016-07-14 10:05:07 +0200

    max italian alphabet chars getted from control string

--- source/CodiceFiscale.js
+++ source/CodiceFiscale.js
@@ -1,6 +1,6 @@
 /**
  * CodiceFiscale
- * 
+ *
  * A javscript object for managing the italian tax code:
  * - create it
  * - check it
@@ -19,10 +19,6 @@ var CodiceFiscale = CodiceFiscale || {};
     /** @private */
     /** @type {Number} The number of max chars on italian tax code */
     CodiceFiscale.maxChar = 15;
-
-    /** @private */
-    /** @type {Number} The number of italian alfabet chars */
-    CodiceFiscale.maxAlphabetChar = 26;
 
     /** @private */
     /** @type {Array} The array of month in char */
@@ -53,7 +49,7 @@ var CodiceFiscale = CodiceFiscale || {};
         'odd': {
             0:1,  1:0,  2:5,  3:7,  4:9,  5:13, 6:15, 7:17, 8:19,
             9:21, A:1,  B:0,  C:5,  D:7,  E:9,  F:13, G:15, H:17,
-            I:19, J:21, K:2,  L:4,  M:18, N:20, O:11, P:3,  Q:6, 
+            I:19, J:21, K:2,  L:4,  M:18, N:20, O:11, P:3,  Q:6,
             R:8,  S:12, T:14, U:16, V:10, W:22, X:25, Y:24, Z:23
         }
     };
@@ -252,7 +248,7 @@ var CodiceFiscale = CodiceFiscale || {};
 
     /**
      * Get the vowels of a string
-     * 
+     *
      * @param  {String} string The string to parse
      * @return {String}
      */
@@ -313,7 +309,7 @@ var CodiceFiscale = CodiceFiscale || {};
      * @return {Number}
      */
     CodiceFiscale.getMaxAlphabetChar = function () {
-        return this.maxAlphabetChar;
+        return this.getControlChars().length;
     };
 
     /**
