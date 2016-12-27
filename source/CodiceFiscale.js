1119fafc302b67bb9ee400638b0a9d40be8b1785
Andrea Cognini <info@heavybeard.it>
2016-12-27 09:58:54 +0100

    prettify js

--- source/CodiceFiscale.js
+++ source/CodiceFiscale.js
@@ -72,16 +72,80 @@ CodiceFiscale.prototype.__settings = {
     /** @type {Object} Table of association of char to control */
     charsControlValue: {
         'even': {
-            0:0,  1:1,   2:2,  3:3,   4:4,  5:5,  6:6,  7:7,  8:8,
-            9:9,  A:0,   B:1,  C:2,   D:3,  E:4,  F:5,  G:6,  H:7,
-            I:8,  J:9,   K:10, L:11,  M:12, N:13, O:14, P:15, Q:16,
-            R:17, S:18,  T:19, U:20,  V:21, W:22, X:23, Y:24, Z:25
+            0: 0,
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 4,
+            5: 5,
+            6: 6,
+            7: 7,
+            8: 8,
+            9: 9,
+            A: 0,
+            B: 1,
+            C: 2,
+            D: 3,
+            E: 4,
+            F: 5,
+            G: 6,
+            H: 7,
+            I: 8,
+            J: 9,
+            K: 10,
+            L: 11,
+            M: 12,
+            N: 13,
+            O: 14,
+            P: 15,
+            Q: 16,
+            R: 17,
+            S: 18,
+            T: 19,
+            U: 20,
+            V: 21,
+            W: 22,
+            X: 23,
+            Y: 24,
+            Z: 25
         },
         'odd': {
-            0:1,  1:0,  2:5,  3:7,  4:9,  5:13, 6:15, 7:17, 8:19,
-            9:21, A:1,  B:0,  C:5,  D:7,  E:9,  F:13, G:15, H:17,
-            I:19, J:21, K:2,  L:4,  M:18, N:20, O:11, P:3,  Q:6,
-            R:8,  S:12, T:14, U:16, V:10, W:22, X:25, Y:24, Z:23
+            0: 1,
+            1: 0,
+            2: 5,
+            3: 7,
+            4: 9,
+            5: 13,
+            6: 15,
+            7: 17,
+            8: 19,
+            9: 21,
+            A: 1,
+            B: 0,
+            C: 5,
+            D: 7,
+            E: 9,
+            F: 13,
+            G: 15,
+            H: 17,
+            I: 19,
+            J: 21,
+            K: 2,
+            L: 4,
+            M: 18,
+            N: 20,
+            O: 11,
+            P: 3,
+            Q: 6,
+            R: 8,
+            S: 12,
+            T: 14,
+            U: 16,
+            V: 10,
+            W: 22,
+            X: 25,
+            Y: 24,
+            Z: 23
         },
     },
     /** @type {JSON} Defined in CodiceFiscale.cadastralCodes.js */
@@ -142,7 +206,7 @@ CodiceFiscale.prototype.nameCode = function () {
     else {
         nameCode += this.__vowels(name);
         nameCode += 'XXX';
-        nameCode = nameCode.substr(0,3);
+        nameCode = nameCode.substr(0, 3);
     }
 
     return nameCode.toUpperCase();
@@ -360,7 +424,7 @@ CodiceFiscale.prototype._options = function (generality) {
  * @return {String}
  */
 CodiceFiscale.prototype.__consonants = function (string) {
-    return string.replace(/[^BCDFGHJKLMNPQRSTVWXYZ]/gi,'');
+    return string.replace(/[^BCDFGHJKLMNPQRSTVWXYZ]/gi, '');
 };
 
 /**
@@ -371,7 +435,7 @@ CodiceFiscale.prototype.__consonants = function (string) {
  * @return {String}
  */
 CodiceFiscale.prototype.__vowels = function (string) {
-    return string.replace(/[^AEIOU]/gi,'');
+    return string.replace(/[^AEIOU]/gi, '');
 };
 
 /**
